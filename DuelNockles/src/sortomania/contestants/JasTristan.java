9c6923bca11231bdeffdc0c84b91b9bad1d7a98d
BT_1N3_27 <BT_1N3_27@BT_1N3_27.bktech.local>
2017-12-11 15:00:22 -0500

    123

---
+++ DuelNockles/src/sortomania/contestants/JasTristan.java
@@ -0,0 +1,41 @@
+package sortomania.contestants;
+
+import java.awt.Color;
+
+import sortomania.Contestant;
+
+public class JasTristan extends Contestant {
+
+	public Color getColor() {
+		return new Color(102,194,255);
+	}
+
+	public String getSpriteName() {
+		return CHUN_LI;
+	}
+
+	public double sortAndGetMedian(int[] random) {
+		return 0;
+	}
+
+	public int sortAndGetResultingIndexOf(String[] strings, String toFind) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public double mostlySortAndGetMedian(int[] mostlySorted) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public double sortMultiDim(int[][] grid) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int sortAndSearch(Comparable[] arr, Comparable toFind) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
